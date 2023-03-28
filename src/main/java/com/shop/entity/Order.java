f383f1ec3b5879bfa3217acd33ad9f538e6a93b0
BarnabasKim <dowon8618@gmail.com>
2023-03-28 23:04:57 +0900

    지연 로딩 및 고아객체 제거 테스트완

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -19,16 +19,18 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    private LocalDateTime orderDate;
+    private LocalDateTime orderDate; // 주문일
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
+    private OrderStatus orderStatus; // 주문싱태
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL,orphanRemoval = true,
+    fetch = FetchType.LAZY)
+
     private List<OrderItem> orderItems = new ArrayList<>();
 
 
